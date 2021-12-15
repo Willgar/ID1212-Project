4ac96bc0785bb70f40aa4d9898833022069a0253
William <Williamaxb@gmail.com>
2021-12-15 16:19:51 +0100

    refactor; Moving database access to new DA Layer

--- src/main/java/se/kth/id1212/taxoptimization/model/Session.java
+++ src/main/java/se/kth/id1212/taxoptimization/model/Session.java
@@ -1,7 +1,8 @@
 package se.kth.id1212.taxoptimization.model;
 
+import se.kth.id1212.taxoptimization.data_access.SECRETS;
+
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -88,18 +89,5 @@ public class Session {
     public int[][] getYearlyCSNCapital(){
         return this.inputs.get(inputs.size()-1).getYearlyCSNCapital();
     }
-    private void SQLQueryGet(String query) throws Exception{
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test" , SECRETS.DATABASEUSER,  SECRETS.DATABASEPASS);
-        Statement stmt=con.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while(rs.next()){
-            String input_id = rs.getString(1);
-            int start_capital = rs.getInt(2);
-            int profit_capital = rs.getInt(3);
-            int interest_rate = rs.getInt(4);
-            int years = rs.getInt(5);
-            //inputs.add(new Input(start_capital, profit_capital,interest_rate, years));
-        }
-    }
+
 }
--- src/main/java/se/kth/id1212/taxoptimization/model/User.java
+++ src/main/java/se/kth/id1212/taxoptimization/model/User.java
@@ -1,5 +1,7 @@
 package se.kth.id1212.taxoptimization.model;
 
+import se.kth.id1212.taxoptimization.data_access.SECRETS;
+
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -99,67 +101,5 @@ public class User {
     public void createCSNInput(int total_loan, int interest_rate, int desired_payments ){
         this.sessions.get(sessions.size()-1).updateCSNInput(total_loan, interest_rate, desired_payments);
     }
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     *  Compares the email and password combo with the database to see if the user already exists, and if not it
-     *  creates a new user.
-     * @return True if user exists, false if it did not.
-     */
-    public boolean userExists() throws SQLException, ClassNotFoundException {
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project" , SECRETS.DATABASEUSER,  SECRETS.DATABASEPASS);
-            Statement stmt=con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT email, password FROM User WHERE email='"+this.email+"';");
-            rs.next();
-            String password = rs.getString(2);
-            System.out.println(password + " and " + this.password);
-            if(password.equals(this.password)) {
-                System.out.println("hello");
-                return true;
-            }
-            else {
-                System.out.println("hello2");
-
-                return false;   //Email exists but password did not match
-            }
-        } catch (Exception e){
-            System.out.println(e);
-            try { //Redo when DB is finished
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", SECRETS.DATABASEUSER, SECRETS.DATABASEPASS);
-                String query = " INSERT INTO User (email, name, password)" + " values (?,?,?)";
-                PreparedStatement prep = con.prepareStatement(query);
-                prep.setString(0, this.email);
-                prep.setString(1, this.firstname);
-                prep.setString(2, this.password);
-                prep.execute();
-                con.close();
-            } catch(Exception e2){return false;}
-            return false;
-        }
-    }
-    private void SQLQueryGet(String query) throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project" , SECRETS.DATABASEUSER,  SECRETS.DATABASEPASS);
-        Statement stmt=con.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while(rs.next()){
-            String session_id = rs.getString(1);
-            String time = rs.getString(2);
-            String location = rs.getString(3);
-            sessions.add(new Session(session_id, time, location));
-        }
-        con.close();
-    }
+    
 }
